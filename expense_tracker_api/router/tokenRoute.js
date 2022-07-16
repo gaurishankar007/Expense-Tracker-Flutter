006ed5b72cbf337b4821ae20bc42e79f02ea6e42
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2022-07-16 18:44:45 +0545

    Separating App from backend

--- expense_tracker_api/router/tokenRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = new express.Router();
-const { generateToken, verifyToken } = require("../controller/tokenController");
-
-router.post("/generate", generateToken);
-
-router.put("/verify", verifyToken);
-
-module.exports = router;
